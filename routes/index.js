394e036fb6111c2847c0accca8aacf6b286073af
Andrei Costea <andreicst.97@gmail.com>
2023-03-06 11:48:09 +0100

    first config

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+
+const authRoutes = require('./auth.routes')
+router.use('/auth', authRoutes)
+
+const productsRoutes = require('./products.routes')
+router.use('/products', productsRoutes)
+
+const userRoutes = require('./user.routes')
+router.use('/user', userRoutes)
+
+
+module.exports = router;
