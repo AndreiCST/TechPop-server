e2bc73d4c563d6b2a60316b3fe61c8bd7e4b27ee
Miguel Ferragut <mferracab@gmail.com>
2023-03-06 15:11:08 +0100

    after conflicts

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const router = require("express").Router()
 // const authRoutes = require('./auth.routes')
 // router.use('/auth', authRoutes)
 
-// const productsRoutes = require('./products.routes')
-// router.use('/products', productsRoutes)
+const productsRoutes = require('./products.routes')
+router.use('/products', productsRoutes)
 
 // const userRoutes = require('./user.routes')
 // router.use('/user', userRoutes)
