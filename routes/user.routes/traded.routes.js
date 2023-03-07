5fb30bf394a29182cc95ede8da557bf338c7c070
Andrei Costea <andreicst.97@gmail.com>
2023-03-07 10:51:40 +0100

    traded routes started

--- routes/user.routes/traded.routes.js
+++ routes/user.routes/traded.routes.js
@@ -1,8 +1,17 @@
 const router = require('express').Router()
+const User = require('./../../models/User.model')
 const Product = require('./../../models/Product.model')
 const Category = require('./../../models/Category.model')
 const Subcategory = require('./../../models/Subcategory.model')
 
+router.get('/selling/:user_id', (req, res, next) => {
+    const { user_id } req.params
+
+    User
+        .findById(user_id)
+        .then(products => res.send(products))
+        .catch(err => next(err))
+})
 
 
 module.exports = router
